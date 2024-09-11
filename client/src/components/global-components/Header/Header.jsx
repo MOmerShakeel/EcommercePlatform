6a4823af6eefbdb909276069b9c8804043ad1a73
Omer Shakeel <omershakeel742@gmail.com>
2024-09-11 21:50:09 +0500

    Header creation

--- client/src/components/global-components/Header/Header.jsx
+++ client/src/components/global-components/Header/Header.jsx
@@ -1,5 +1,34 @@
+import logo from "../../../assets/logo.png";
+import "../Header/Header.css";
+
 function Header() {
-    return <></>
+    return (
+        <div className="header-main">
+            <div className="header-sub-section">
+                <div className="header-logo-div">
+                    <img src={logo} className="header-logo" alt="logo" />
+                </div>
+                <div className="header-menu-div">
+                    <nav>
+                        <li>
+                            <ul>
+                                Home
+                            </ul>
+                            <ul>
+                                Home
+                            </ul>
+                            <ul>
+                                Home
+                            </ul>
+                            <ul>
+                                Home
+                            </ul>
+                        </li>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header;
