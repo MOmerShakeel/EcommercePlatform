b517325be5a9e0b48e5ea102c2810f045dd2e4f1
M Omer Shakeel <omershakeel742@gmail.com>
2024-10-07 23:08:17 +0500

    Text Animation Component Edits

--- client/src/components/home-components/TextAnimation/TextAnimation.jsx
+++ client/src/components/home-components/TextAnimation/TextAnimation.jsx
@@ -1,6 +1,27 @@
+import "../TextAnimation/TextAnimation.css"
+import barIcon from "../../../assets/carticon.png"
+
+
 function TextAnimation(){
     return (
-    <></>
+    <>
+    <div className="textanimation-main-div">
+        <div className="textanimation-primary">
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+            <div className="textanimation-tagline">Upto 20% off on all products!</div>
+            <div className="textanimation-icon"><img src={barIcon} /></div>
+        </div>
+    </div>
+    </>
 );
 }
 
