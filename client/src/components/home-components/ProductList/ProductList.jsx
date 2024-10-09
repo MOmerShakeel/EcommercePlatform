92764467babd95ac7cb2226d01b2d0de5dcb223b
M Omer Shakeel <omershakeel742@gmail.com>
2024-10-09 21:39:59 +0500

    Product List Component Subsection

--- client/src/components/home-components/ProductList/ProductList.jsx
+++ client/src/components/home-components/ProductList/ProductList.jsx
@@ -2,7 +2,12 @@ import "../ProductList/ProductList.css"
 
 function ProductList(){
     return (
-    <></>
+    <>
+    <div className="productlist-main">
+        <div className="productlist-featured-section"></div>
+        <div className="productlist-viewmore-button"></div>
+    </div>
+    </>
 );
 }
 
