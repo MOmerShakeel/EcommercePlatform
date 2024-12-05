4768cb4cd5153d0fa8f56e74b44cf9595e4f2757
M Omer Shakeel <omershakeel742@gmail.com>
2024-12-05 21:06:13 +0500

    Footer Div Commit

--- client/src/components/global-components/Footer/Footer.jsx
+++ client/src/components/global-components/Footer/Footer.jsx
@@ -2,14 +2,18 @@ import logo from "../../../assets/logo.png"
 
 function Footer() {
     return <>
-    <div>
-        <div><img src={logo}/></div>
-        <div><h3>Menu</h3></div>
-        <div><ul>
-            <li>Facebook</li>
-            <li>Instagram</li>
+        <div>
+            <div><img src={logo} /></div>
+            <div><h3>Important Links</h3>
+                <ul><li>About</li>
+                    <li>Contact</li>
+                    <li>Privacy Policy</li></ul>
+            </div>
+            <div><ul>
+                <li>Facebook</li>
+                <li>Instagram</li>
             </ul></div>
-    </div>
+        </div>
     </>
 }
 
